e4ad791cf5b13e6ca142e10f13ab708dea25529f
Carlos Guzmán Velasco <carlos.guzman.ext@orangebank.es>
2021-06-11 12:31:32 +0200

    Modifying log message

--- commons/steps/get_environment_data.groovy
+++ commons/steps/get_environment_data.groovy
@@ -2,6 +2,6 @@ Object call(String environment_name) {
     log.debug("binding: " + binding)
     log.debug("binding.variables: " + binding.variables)
     log.debug("binding.getProperties(): " + binding.getProperties())
-    log.debug("jte.application_environments:" + jte.application_environments)
+    log.debug("jte.application_environments: " + jte.application_environments)
     return binding.getVariable(environment_name)
 }
