1112d11c96184deda00972d6081a55d01296cbd8
Carlos Guzmán Velasco <carlos.guzman.ext@orangebank.es>
2021-06-11 10:45:32 +0200

    Removing debug trace

--- commons/steps/get_environment_data.groovy
+++ commons/steps/get_environment_data.groovy
@@ -1,7 +1,6 @@
 Object call(String environment_name) {
     log.debug(binding)
     log.debug(binding.variables)
-    log.debug(binding.toString())
     log.debug(binding.getProperties())
     return binding.getVariable(environment_name)
 }
