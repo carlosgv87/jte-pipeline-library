0a2e7acc7c33f9480aa1f76a62ab52a1f373a2c6
Carlos Guzmán Velasco <carlos.guzman.ext@orangebank.es>
2021-06-11 10:32:07 +0200

    Adding debug traces

--- commons/steps/get_environment_data.groovy
+++ commons/steps/get_environment_data.groovy
@@ -1,5 +1,7 @@
 Object call(String environment_name) {
     log.debug(binding)
     log.debug(binding.variables)
+    log.debug(binding.toString())
+    log.debug(binding.getProperties())
     return binding.getVariable(environment_name)
 }
