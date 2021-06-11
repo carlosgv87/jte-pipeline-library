cd8d66b4ae96434066854f13ae06c87aa5386a0b
Carlos Guzmán Velasco <carlos.guzman.ext@orangebank.es>
2021-06-11 12:04:00 +0200

    Adding more infor to log traces

--- commons/steps/get_environment_data.groovy
+++ commons/steps/get_environment_data.groovy
@@ -1,7 +1,7 @@
 Object call(String environment_name) {
-    log.debug(binding)
-    log.debug(binding.variables)
-    log.debug(binding.getProperties())
-    log.debug(jte.application_environments)
+    log.debug("binding: " + binding)
+    log.debug("binding.variables: " + binding.variables)
+    log.debug("binding.getProperties(): " + binding.getProperties())
+    log.debug("jte.application_environments:" + jte.application_environments)
     return binding.getVariable(environment_name)
 }
