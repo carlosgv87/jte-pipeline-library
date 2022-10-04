f2d91e0eb55bd01f48cdd03bc49498c8a39ca4c9
Carlos Guzmán Velasco <carlos.guzman.ext@orangebank.es>
2022-10-04 13:20:38 +0200

    Fixing error on step

--- commons/steps/check_kiuwan_qg.groovy
+++ commons/steps/check_kiuwan_qg.groovy
@@ -1,4 +1,11 @@
 void call(String appName) {
+    def veryLowVulnerabilities = 0
+    def lowVulnerabilities = 0
+    def normalVulnerabilities = 0
+    def highVulnerabilities = 0
+    def veryHighVulnerabilities = 0
+    def totalVulnerabilities = 0
+
     echo """
     Analysis summary:
      - Security.Vulnerabilities.VeryLow: ${veryLowVulnerabilities}
